0af256f7b715af4067085abf7dbff333797da4e3
konth <konth@LAPTOP-OMJBUG06>
2020-04-12 13:21:53 +0530

    Email configarations

---
+++ 07SprintBootJDBCApp/src/main/java/com/vidvaan/spring/boot/sprintbootapp/controller/SimpleMailController.java
@@ -0,0 +1,26 @@
+package com.vidvaan.spring.boot.sprintbootapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mail")
+public class SimpleMailController {
+
+	@Autowired
+	private JavaMailSender mailSender;
+
+	@GetMapping(value = "/sendemail")
+	public String SendEmail() {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo("amarlapudichandrakanth@gmail.com");
+		message.setSubject("TestMail");
+		message.setText("Test Mail From Java");
+		mailSender.send(message);
+		return "successfull sent email";
+	}
+}
